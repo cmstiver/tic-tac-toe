f0295d2ffcc4a58fa7d2ba7f1ad095aa5f2d59e9
Chayse Stiver <cmstiver1@gmail.com>
2021-06-21 00:05:18 -0700

    x/o now display on the game board

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const ttt = (() => {
 const game = {
     gameBoard: ['x','o','x','o','x','o','x','o','x'],
 }
+return {
+    game,
+}
 })();
 
 const Player = () => {
-
 }
 
 const display = (() => {
-
-    function game() {
-        
+    function displayMarkers() {
+        ttt.game.gameBoard.forEach((elem, i) => {
+            let marker = document.createElement('div');
+            if (elem == 'x') {
+                marker.classList.add('marker', 'X');
+                marker.textContent = "X"
+            } else if (elem == 'o') {
+                marker.classList.add('marker', 'O');
+                marker.textContent = "O"
+            } else {
+                return
+            }
+            let square = document.getElementById(`${i}`)
+            square.appendChild(marker)
+        })
     }
-    return {
-        game,
+    function test() {
+        console.log(ttt.game)
     }
+return {
+    displayMarkers,
+    test,
+}
 })();
 
-display.game()
+display.displayMarkers()
