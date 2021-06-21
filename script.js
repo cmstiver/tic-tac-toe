c4c1062d5c65e37ee9e2d62fa83c7859d7eb22c0
Chayse Stiver <cmstiver1@gmail.com>
2021-06-20 23:21:51 -0700

    create x/o markers

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const ttt = (() => {
 const game = {
-    gameBoard: [x,o,x,o,x,o,x,o,x],
+    gameBoard: ['x','o','x','o','x','o','x','o','x'],
 }
 })();
 
 const Player = () => {
 
-
-
 }
+
+const display = (() => {
+
+    function game() {
+        
+    }
+    return {
+        game,
+    }
+})();
+
+display.game()
